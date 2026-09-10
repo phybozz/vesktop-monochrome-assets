3da1c0c3621cadb589df4c1376d22b66cfea6a46
phybozz <alexanderbeast2.0@gmail.com>
2026-09-10 18:33:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,15 +4,16 @@ if [ -d "$HOME/.config/vesktop" ]; then
 elif [ -d "$HOME/.var/app/dev.vencord.Vesktop/config/vesktop" ]; then
     AD="$HOME/.var/app/dev.vencord.Vesktop/config/vesktop/userAssets"
 else
-    echo "Open an issue in the GitHub repository, thanks! :("
+    echo "Error: Open an issue in the GitHub repository, thanks! :("
     exit 1
 fi
+pkill vesktop
 mkdir -p "$AD"
-curl -f# "https://raw.githubusercontent.com/phybozz/vesktop-monochrome-assets/refs/heads/main/assets/splash.webp" -o "$AD/splash.webp" \
+curl -f# "https://raw.githubusercontent.com/phybozz/vesktop-monochrome-assets/refs/heads/main/assets/splash.webp" -o "$AD/splash" \
 && echo "Installed splash.webp successfully."
-curl -f# "https://raw.githubusercontent.com/phybozz/vesktop-monochrome-assets/refs/heads/main/assets/tray.png" -o "$AD/tray.png" \
+curl -f# "https://raw.githubusercontent.com/phybozz/vesktop-monochrome-assets/refs/heads/main/assets/tray.png" -o "$AD/tray" \
 && echo "Installed tray.png successfully."
-curl -f# "https://raw.githubusercontent.com/phybozz/vesktop-monochrome-assets/refs/heads/main/assets/trayUnread.png" -o "$AD/trayUnread.png" \
+curl -f# "https://raw.githubusercontent.com/phybozz/vesktop-monochrome-assets/refs/heads/main/assets/trayUnread.png" -o "$AD/trayUnread" \
 && echo "Installed trayUnread.png successfully."
 echo "
 Done! :D"
